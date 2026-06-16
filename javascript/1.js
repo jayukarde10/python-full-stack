11d3ce52e8dbfd18461331a308a24b204b073669
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-06-17 00:38:20 +0530

    java script

---
+++ javascript/1.js
@@ -0,0 +1,14 @@
+document.write("hello")
+console.log("hi")
+
+
+
+function msg(){ 
+    alert("hello! thanks for saying hello back"); 
+    } 
+
+function hello(){
+    return ("hello by js")
+}
+
+document.write(hello())
